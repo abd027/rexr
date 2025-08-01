32bb1586f5a9bd86686497dd5fa0b690ba4e182a
Abdullah <abdullah.work027@gmail.com>
2025-08-01 11:51:28 +0500

    Fetch the genres.

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -4,21 +4,28 @@ import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 
-
 const GameGrid = () => {
-   const  {games, error, isLoading} = useGames();
-   const skeletons = [1,2,3,4,5,6];
+  const { games, error, isLoading } = useGames();
+  const skeletons = [1, 2, 3, 4, 5, 6];
   return (
-   <>
-   {error && <Text>{error}</Text>}
-    <SimpleGrid columns={{sm: 1, md: 2, lg: 3}} spacing="10" padding="10px">    
-      {isLoading && skeletons.map((skeleton)=>(<GameCardContainer><GameCardSkeleton/></GameCardContainer>))}
-      <GameCardContainer>
-        {games.map((game)=> (<GameCard  key={game.id} game={game}/>))}
-        </GameCardContainer>
-    </SimpleGrid>
-   </>
-  )
-}
+    <>
+      {error && <Text>{error}</Text>}
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing="10" padding="10px">
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GameCardContainer>
+              <GameCardSkeleton key={skeleton} />
+            </GameCardContainer>
+          ))}
 
-export default GameGrid
+        {games.map((game) => (
+          <GameCardContainer>
+            <GameCard key={game.id} game={game} />
+          </GameCardContainer>
+        ))}
+      </SimpleGrid>
+    </>
+  );
+};
+
+export default GameGrid;
