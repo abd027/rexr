666d111ffb04b91976994aeeb1b4b4188129a036
Abdullah <abdullah.work027@gmail.com>
2025-08-01 18:21:58 +0500

    Filter games by platform.

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,9 +1,9 @@
 import useData from "./useData";
 import { Genre } from "./useGenres";
 
-export interface Platform{
-  id:number;
-  name:string;
+export interface Platform {
+  id: number;
+  name: string;
   slug: string;
 }
 
@@ -11,9 +11,17 @@ export interface Game {
   id: number;
   name: string;
   background_image: string;
-  parent_platforms: {platform: Platform}[];
+  parent_platforms: { platform: Platform }[];
   metacritic: number;
 }
 
-const useGames = (selectedGenre: Genre | null) => useData<Game>("/games", {params: {genres: selectedGenre?.id}},[selectedGenre?.id]);
+const useGames = (
+  selectedGenre: Genre | null,
+  selectedPlatform: Platform | null
+) =>
+  useData<Game>(
+    "/games",
+    { params: { genres: selectedGenre?.id, platforms: selectedPlatform?.id } },
+    [selectedGenre?.id, selectedPlatform?.id]
+  );
 export default useGames;
