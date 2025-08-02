b5563f7aec91fd8299853bcc13b9acd36157f4f1
Abdullah <abdullah.work027@gmail.com>
2025-08-02 08:15:36 +0500

    Clean up the game cards.

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,31 +1,31 @@
-import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react"
+import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import { getMixer } from "framer-motion";
 import getCroppedImageURL from "../hooks/imageUrls";
 
-interface Props{
-    game: Game;
+interface Props {
+  game: Game;
 }
 
-const GameCard = ({game}:Props) => {
+const GameCard = ({ game }: Props) => {
   return (
     <>
-     <Card >
-        <Image src={getCroppedImageURL(game.background_image)}  />
+      <Card>
+        <Image src={getCroppedImageURL(game.background_image)} />
         <CardBody>
-            <Heading fontSize="2xl">
-                {game.name}
-            </Heading>
-            <HStack justifyContent="space-between">
-            <PlatformIconList platforms={game.parent_platforms.map((p)=>p.platform)}/>
-                <CriticScore score={game.metacritic}/>
-                </HStack>
+          <HStack justifyContent="space-between" marginBottom={3}>
+            <PlatformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
+          <Heading fontSize="2xl">{game.name}</Heading>
         </CardBody>
-        </Card> 
+      </Card>
     </>
-  )
-}
+  );
+};
 
-export default GameCard
+export default GameCard;
